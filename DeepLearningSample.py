e0979e5c32fa01b53df96292d5fecb008a2190e3
Aryan <aryanchittora09@gmail.com>
2022-04-04 14:46:01 +0530

    Work of Class 232

--- DeepLearningSample.py
+++ DeepLearningSample.py
@@ -14,3 +14,9 @@ model.add(Dense(12, input_dim=8, activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.summary()
+
+model.compile(loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x, y, epochs=500, batch_size=100)
+predictions = model.predict(x)
+for i in range(5):
+    print(f'{x[i].tolist()} => {predictions[i].round()} expected {y[i]}')
