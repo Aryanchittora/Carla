9bbb1f4f0bbc119a7985e48ecc8daee5081e38c5
Aryan <aryanchittora09@gmail.com>
2022-04-12 14:21:20 +0530

    Project-233 Work

---
+++ Project-233.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = pd.read_csv('student_data.csv')
+
+data['Total_marks_obtained'] = data.iloc[:, [2, 3, 4]].sum(axis=1)
+data.loc[data['Total_marks_obtained'] > 250, 'Grade'] = 'A'
+data.loc[data['Total_marks_obtained'] < 250, 'Grade'] = 'B'
+data['Percentage'] = data['Total_marks_obtained'] / data['Overall_Total'] * 100
+
+print(data)
